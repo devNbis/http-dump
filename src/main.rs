74958537fe5454a1b803d5a41d820ff4217c4f8a
nbis Klaus Bertram <dev@n-bis-de>
2026-01-15 12:32:00 +0100

    use clap and env

--- src/main.rs
+++ src/main.rs
@@ -8,42 +8,54 @@
 
 use axum::{
     Router,
-     body::{Body, Bytes}, 
-
-    extract::{ 
-       Request, State,Path,
-      ws::{ WebSocketUpgrade,WebSocket,Message
-      }}, 
-      http::{header,StatusCode },
-      debug_middleware,
-       middleware::{self, Next},
-     response::{IntoResponse, Response, Html}, routing::{any,get}
-    
+    body::{Body, Bytes},
+    debug_middleware,
+    extract::{
+        Path, Request, State,
+        ws::{Message, WebSocket, WebSocketUpgrade},
+    },
+    http::{StatusCode, header},
+    middleware::{self, Next},
+    response::{Html, IntoResponse, Response},
+    routing::{any, get},
 };
 
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-use tokio::signal;
-use tokio::sync::broadcast;
-use tokio::sync::Mutex;
-use tokio::sync::broadcast::Sender;
-use tower::ServiceBuilder;
-use std::{
-   sync::{Arc },
-  env};
 use http_body_util::BodyExt;
 use rust_embed::Embed;
+use std::{collections::HashMap,  sync::Arc};
+use tokio::signal;
+use tokio::sync::Mutex;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::Sender;
+use tower::ServiceBuilder;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use clap::Parser;
 
+/// Programm for mapping of json via jolt
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Binding of the web server
+    #[arg(short, long, value_name = "IP:Port", env="HTTP_DUMP_BIND",default_value ="0.0.0.0:8089", help="host address include port")]
+    bind: String,
 
-// Our shared state
-struct AppState {
-  count:Mutex<i32>,
-   err400_count:i32,
-   err500_count:i32,
-  
-    tx: broadcast::Sender<String>,
+    /// Error map definition
+    #[arg(short, long, value_name = "MAP",env="HTTP_DUMP_ERROR_MAP",default_value ="", help="string as mapp with <count>:<error> delimiter ; \nsample: \"4:500,6:400\"\nresponed with error on every 4th call with 500 and every 6th call with 400")]
+    error_map: String,
+
+    /// logging definition
+    #[arg(short, long, value_name = "LOG",env="HTTP_DUMP_TRACELOG",default_value ="info,tower=info", help="logging definition")]
+    tracelog: String
 }
 
+// Our shared state
+#[derive(Debug)]
+struct AppState {
+    count: Mutex<i32>,
+    error_map: HashMap<i32, StatusCode>,
 
+    tx: broadcast::Sender<String>,
+}
 
 #[derive(Embed, Clone)]
 #[folder = "assets/"]
@@ -51,48 +63,76 @@ struct Asset;
 
 #[tokio::main]
 async fn main() {
+     let cli = Cli::parse();
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+                cli.tracelog.into()
             }),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let bind_address = env::var("HTTP_DUMP_BIND").unwrap_or("0.0.0.0:8089".to_string());
-    let err400_count: i32= env::var("HTTP_DUMP_ERR400_COUNT").unwrap_or("0".to_string()).parse().expect("Failed env var.");
-    let err500_count: i32= env::var("HTTP_DUMP_ERR500_COUNT").unwrap_or("0".to_string()).parse().expect("Failed env var.");
-   
+    let bind_address = cli.bind;
+
+    let error_map = build_error_map(cli.error_map);
+
     let (tx, _rx) = broadcast::channel(100);
 
-    let app_state = Arc::new(AppState {count:Mutex::new(0), err400_count ,err500_count,tx });
-
+    let app_state = Arc::new(AppState {
+        count: Mutex::new(0),
+        error_map,
+        tx,
+    });
+    tracing::debug!("AppState:  {:#?} ", app_state);
     let app = Router::new()
-       .fallback_service(get(not_found))
+        .fallback_service(get(not_found))
         .route("/ws", get(websocket_handler))
         .route("/{*wildcard}", any(|| async move { /* ... */ }))
-      .route("/", get(index_handler))
-    .route("/index.html", get(index_handler))
-    .route("/favicon.ico", get(favicon_handler))
-    .layer(
-        ServiceBuilder::new()
-       .layer(middleware::from_fn_with_state(app_state.clone(),print_request_response))
-       .layer(middleware::from_fn_with_state(app_state.clone(),response_on_error_counts))
-    )
-       .with_state(app_state);
+        .route("/", get(index_handler))
+        .route("/index.html", get(index_handler))
+        .route("/favicon.ico", get(favicon_handler))
+        .layer(
+            ServiceBuilder::new()
+                .layer(middleware::from_fn_with_state(
+                    app_state.clone(),
+                    print_request_response,
+                ))
+                .layer(middleware::from_fn_with_state(
+                    app_state.clone(),
+                    response_on_error_counts,
+                )),
+        )
+        .with_state(app_state);
 
-
-    let listener = tokio::net::TcpListener::bind(bind_address)
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(bind_address).await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     #[allow(unused)]
     axum::serve(listener, app)
-    .with_graceful_shutdown(shutdown_signal())
-    .await;
+        .with_graceful_shutdown(shutdown_signal())
+        .await;
 }
 
+fn build_error_map(data_str: String) -> HashMap<i32, StatusCode> {
+    let mut map = HashMap::new();
+    if data_str.is_empty() {
+        return map;
+    }
+    let token: Vec<&str> = if data_str.contains(";") {
+        data_str.split(";").collect()
+    } else {
+        vec![data_str.as_str()]
+    };
+    for token_str in token {
+        let val: Vec<&str> = token_str.split(":").collect();
+        if val.len() == 2 {
+            let key = val[0].parse::<i32>().unwrap();
+            let code: StatusCode = StatusCode::from_bytes(val[1].as_bytes()).unwrap();
+            map.entry(key).insert_entry(code);
+        }
+    }
+    map
+}
 
 async fn websocket_handler(
     ws: WebSocketUpgrade,
@@ -108,7 +148,7 @@ async fn websocket(mut stream: WebSocket, state: Arc<AppState>) {
     // By splitting, we can send and receive at the same time.
     //let (mut sender, mut receiver) = stream.split();
     let mut rx = state.tx.subscribe();
-   let mut _send_task = tokio::spawn(async move {
+    let mut _send_task = tokio::spawn(async move {
         while let Ok(msg) = rx.recv().await {
             // In any websocket error, break loop.
             if stream.send(Message::text(msg)).await.is_err() {
@@ -116,91 +156,66 @@ async fn websocket(mut stream: WebSocket, state: Arc<AppState>) {
             }
         }
     });
-
 }
-
-
 
 // Finally, we use a fallback route for anything that didn't match.
 async fn not_found() -> Html<&'static str> {
-  Html("<h1>404</h1><p>Not Found</p>")
+    Html("<h1>404</h1><p>Not Found</p>")
 }
 
 #[debug_middleware]
 async fn response_on_error_counts(
- State(state): State<Arc<AppState>>,
+    State(state): State<Arc<AppState>>,
     req: Request,
     next: Next,
-    
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     let mut count = state.count.lock().await;
     *count += 1;
-     tracing::debug!("request counter: {}, 400er: {}, 500er: {}",*count, state.err400_count, state.err500_count);
-    if is_factor( state.err400_count ,*count) {
-      
+    tracing::debug!("request counter: {}", *count);
 
-            return Err((
-                StatusCode::BAD_REQUEST,
-                "".to_string()),
-            )
-       ;
+    for err_map in &state.clone().error_map {
+        if is_factor(*err_map.0, *count) {
+            return Ok(err_map.1.into_response());
+        }
     }
-    if is_factor( state.err500_count ,*count) {
-            return Err((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "".to_string()),
-            )
-          ;
-    }
-    //let (parts, body) = req.into_parts();
     let res = next.run(req).await;
-    //let res = Response::from_parts(parts,body);
     Ok(res)
-  
 }
-
 
 #[debug_middleware]
 async fn print_request_response(
-   State(state): State<Arc<AppState>>,
+    State(state): State<Arc<AppState>>,
     req: Request,
     next: Next,
-    
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     let (parts, body) = req.into_parts();
-
-    let bytes = buffer_and_print(format!("Request: {:?}",parts).as_str(), body, state.tx.clone()).await?;
-    /* let mut count = state.count.lock().await;
-     tracing::debug!("request counter: {}, 400er: {}, 500er: {}",*count, state.err400_count, state.err500_count);
-    *count += 1;
-    
-    if is_factor( state.err400_count ,*count) {
-            return Err((
-                StatusCode::BAD_REQUEST,
-                "".to_string()),
-            )
-          ;
-    }
-    if is_factor( state.err500_count ,*count) {
-            return Err((
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "".to_string()),
-            )
-          ;
-    }*/
-
+    let bytes = buffer_and_print(
+        format!("Request: {:?}", parts).as_str(),
+        body,
+        state.tx.clone(),
+    )
+    .await?;
     let req = Request::from_parts(parts, Body::from(bytes));
 
     let res = next.run(req).await;
 
     let (parts, body) = res.into_parts();
-    let bytes = buffer_and_print(format!("Response: {:?}",parts).as_str(), body, state.tx.clone()).await?;
+    let bytes = buffer_and_print(
+        format!("Response: {:?}", parts).as_str(),
+        body,
+        state.tx.clone(),
+    )
+    .await?;
     let res = Response::from_parts(parts, Body::from(bytes));
 
     Ok(res)
 }
 
-async fn buffer_and_print<B>(direction: &str, body: B,  tx: Sender<String>) -> Result<Bytes, (StatusCode, String)>
+async fn buffer_and_print<B>(
+    direction: &str,
+    body: B,
+    tx: Sender<String>,
+) -> Result<Bytes, (StatusCode, String)>
 where
     B: axum::body::HttpBody<Data = Bytes>,
     B::Error: std::fmt::Display,
@@ -217,54 +232,58 @@ where
 
     if let Ok(body) = std::str::from_utf8(&bytes) {
         let _ = tx.send(format!("{direction} body = {body:?}"));
-        tracing::debug!("{direction} body = {body:?}");
+        tracing::info!("{direction} body = {body:?}");
     }
 
     Ok(bytes)
 }
 
-fn is_factor( factor:i32, number:i32) -> bool {
-  if factor == 0  {
-    return false;
-  }
-  number % factor ==0
+// test if number is of factor
+// factor = 2 Number = 4 result true
+// factor = 2 Number = 5 result false
+// factor = 0 false
+fn is_factor(factor: i32, number: i32) -> bool {
+    if factor == 0 {
+        return false;
+    }
+    number % factor == 0
 }
 // We use static route matchers ("/" and "/index.html") to serve our home
 // page.
 async fn index_handler() -> impl IntoResponse {
-  static_handler(Path("index.html".to_string())).await
+    static_handler(Path("index.html".to_string())).await
 }
 
 // We use static route matchers ("/favicon.ico") to serve our home
 // page.
 async fn favicon_handler() -> impl IntoResponse {
-  static_handler(Path("favicon.ico".to_string())).await
+    static_handler(Path("favicon.ico".to_string())).await
 }
 
 // We use a wildcard matcher ("/dist/*file") to match against everything
 // within our defined assets directory. This is the directory on our Asset
 // struct below, where folder = "examples/public/".
 async fn static_handler(Path(path): Path<String>) -> impl IntoResponse {
-  StaticFile(path)
+    StaticFile(path)
 }
 
 pub struct StaticFile<T>(pub T);
 
 impl<T> IntoResponse for StaticFile<T>
 where
-  T: Into<String>,
+    T: Into<String>,
 {
-  fn into_response(self) -> Response {
-    let path = self.0.into();
+    fn into_response(self) -> Response {
+        let path = self.0.into();
 
-    match Asset::get(path.as_str()) {
-      Some(content) => {
-        let mime = mime_guess::from_path(path).first_or_octet_stream();
-        ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
-      }
-      None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(path).first_or_octet_stream();
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
     }
-  }
 }
 
 async fn shutdown_signal() {
